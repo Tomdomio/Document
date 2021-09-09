71b05ce9af0b7618d0c3ade5cb04faaef434381e
Tôm Domino <70497403+Tomdomio@users.noreply.github.com>
2021-09-10 00:57:36 +0700

    :anger:

--- vuex-app/src/App.vue
+++ vuex-app/src/App.vue
@@ -1,15 +1,15 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <addproducts/>
 </template>
 
+
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import addProducts from './components/addProducts.vue';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    addProducts,
   }
 }
 </script>
@@ -19,8 +19,5 @@ export default {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
---
+++ vuex-app/src/components/addProducts.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="container">
+ 
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'addProducts',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
