e8c09763cc0aab812c33f89208fc0ea61b591cde
Tôm Domino <70497403+Tomdomio@users.noreply.github.com>
2021-09-16 15:15:51 +0700

    :anger:

--- vuex-app/src/store/modules/product.js
+++ vuex-app/src/store/modules/product.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
+import swal from 'sweetalert2'
 
 const state = {
-  Products: []
+  Products: [],
 };
 
 const getters = {
@@ -13,15 +14,21 @@ const getters = {
 const actions = {
   async fetchProducts({ commit }) {
     const response = await axios.get('http://localhost:3000/products');
-
     commit('setProducts', response.data);
-  }
-}
+  },
+  async deleteProd({ commit }, id) {
+    await axios.delete('http://localhost:3000/products/'+id);
+    commit('removeProd', id);
+    swal({
 
-const mutations = {
-    setProducts: (state, Products) => (state.Products = Products)
+    })
+  }
 };
 
+const mutations = {
+  setProducts: (state, Products) => (state.Products = Products),
+  removeProd: (state, id) => state.Products = state.Products.filter((product) => product.id !== id),
+};
 
 export default {
     state,
