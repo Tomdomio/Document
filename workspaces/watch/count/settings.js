f644b8d893b48ffa393cd5ecc3c04ffd92d58bac
Tôm Domino <70497403+Tomdomio@users.noreply.github.com>
2021-09-30 09:03:50 +0700

    LH

---
+++ workspaces/watch/count/settings.js
@@ -0,0 +1,13 @@
+[
+  {
+    tab_name: "Count",
+    settings: [
+      {
+        name: "heading",
+        label: "First text",
+        type: "text",
+        value: "Reprizo Is the Repair Service You Can Trust",
+      },
+    ],
+  },
+];
---
+++ workspaces/watch/single/settings.js
@@ -0,0 +1,42 @@
+[
+  {
+    tab_name: "Single",
+    settings: [
+      {
+        name: "image",
+        label: "Single image",
+        type: "single_image",
+        value: {
+          src: "%URL%assets/7.jpg",
+        },
+      },
+      {
+        name: "heading",
+        label: "First text",
+        type: "text",
+        value: "Reprizo Is the Repair Service You Can Trust",
+      },
+      {
+        name: "second",
+        label: "Second text",
+        type: "text",
+        value:
+          "Reprizo is a family-owned professional watch and jewelry repair store chain located in NewYork. We are experts in fixing and repairing watches from world-renowned brands including Rolex, Breitling, Bell & Ross, and many other brands. We offer a wide range of jewelry services and always provide a guarantee for what we do.",
+      },
+      {
+        name: "signature",
+        label: "Single image",
+        type: "single_image",
+        value: {
+          src: "%URL%assets/spacing.png",
+        },
+      },
+      {
+        name: "third",
+        label: "Third text",
+        type: "text",
+        value: "Founder & CEO",
+      },
+    ],
+  },
+];
