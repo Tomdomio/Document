fc2bd1eae3ab9548715ec9d728805a46b033d06e
Tôm Domino <70497403+Tomdomio@users.noreply.github.com>
2021-10-14 01:05:17 +0700

    LH

---
+++ workspaces/moca-01/calltoaction/settings.js
@@ -0,0 +1,59 @@
+[
+  {
+    tab_name: "General",
+    settings: [
+      {
+        name: "title1",
+        label: "Title",
+        type: "text",
+        value: "Men’s",
+      },
+      {
+        name: "img1",
+        label: "Single image",
+        type: "single_image",
+        value: {
+          src: "%URL%assets/1.png",
+          alt: "Call to action",
+        },
+        description: "Recommend Size : 960 x 885px",
+      },
+    ],
+  },
+  {
+    tab_name: "Settings",
+    settings: [
+      {
+        name: "title2",
+        label: "Title",
+        type: "text",
+        value: "Women’s",
+      },
+      {
+        name: "desc",
+        label: "Description",
+        type: "textarea",
+        value: "Thousands of high quality products",
+      },
+      {
+        name: "cta",
+        label: "Link using text",
+        type: "link",
+        value: {
+          href: "#",
+          text: "Discover now",
+        },
+      },
+      {
+        name: "img2",
+        label: "Single image",
+        type: "single_image",
+        value: {
+          src: "%URL%assets/2.png",
+          alt: "Call to action",
+        },
+        description: "Recommend Size : 960 x 885px",
+      },
+    ],
+  },
+];
---
+++ workspaces/moca-01/newsletter/settings.js
@@ -0,0 +1,20 @@
+[
+  {
+    tab_name: "General",
+    settings: [
+      {
+        name: "title",
+        label: "Title",
+        type: "text",
+        value:
+          "“ Class aptent taciti sociosqu ad litora torquent per nostra, per inceptos himenaeos. Suspendisse faucibus sed dolor eget. ”",
+      },
+      {
+        name: "desc",
+        label: "Description",
+        type: "text",
+        value: "Jont Herry",
+      },
+    ],
+  },
+];
