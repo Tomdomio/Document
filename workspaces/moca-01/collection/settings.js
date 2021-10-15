a89ed5487da1aad8d714bf13fb9174536f704fb8
Tôm Domino <70497403+Tomdomio@users.noreply.github.com>
2021-10-16 02:03:03 +0700

    LH

---
+++ workspaces/moca-01/collection/settings.js
@@ -0,0 +1,19 @@
+[
+  {
+    tab_name: "General",
+    settings: [
+      {
+        name: "heading",
+        label: "Heading",
+        type: "text",
+        value: "New Collection",
+      },
+      {
+        name: "description",
+        label: "Desciption",
+        type: "textarea",
+        value: "Summer treding collection for women",
+      },
+    ],
+  },
+];
---
+++ workspaces/moca-01/videopopup/settings.js
@@ -0,0 +1,38 @@
+[
+  {
+    tab_name: "General",
+    settings: [
+      {
+        name: "heading",
+        label: "Heading",
+        type: "text",
+        value: "New Lookbook",
+      },
+      {
+        name: "description",
+        label: "Desciption",
+        type: "textarea",
+        value: "Thousands of high quality products",
+      },
+      {
+        name: "play",
+        label: "Select icon",
+        type: "icon_picker",
+        value: "lni lni-play",
+        options: {
+          source: "css_line_icon",
+          ext_class: "lni",
+        },
+      },
+      {
+        name: "link",
+        label: "Video link",
+        type: "text",
+        value: "https://youtu.be/MGhw6XliFgo",
+        options: {
+          toolbar: false,
+        },
+      },
+    ],
+  },
+];
