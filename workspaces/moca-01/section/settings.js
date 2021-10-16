c49b2b8d2f1c699b1bc69972b30e4973ed74efb4
Tôm Domino <70497403+Tomdomio@users.noreply.github.com>
2021-10-16 15:03:36 +0700

    LH

---
+++ workspaces/moca-01/section/settings.js
@@ -0,0 +1,54 @@
+[
+  {
+    tab_name: "General",
+    settings: [
+      {
+        name: "heading_1",
+        label: "Heading 1",
+        type: "text",
+        value: "Sale off 20%",
+      },
+      {
+        name: "description_1",
+        label: "Desciption 1",
+        type: "textarea",
+        value: "Sale off all product in Clothing",
+      },
+      {
+        name: "img",
+        label: "Single image",
+        type: "single_image",
+        value: {
+          src: "%URL%assets/2.png",
+          alt: "Call to action",
+        },
+      },
+    ],
+  },
+  {
+    tab_name: "Settings",
+    settings: [
+      {
+        name: "heading_2",
+        label: "Heading 2",
+        type: "text",
+        value: "Best Seller",
+      },
+      {
+        name: "description_2",
+        label: "Desciption 2",
+        type: "textarea",
+        value: "Sale off all product in Clothing",
+      },
+      {
+        name: "imgg",
+        label: "Single image",
+        type: "single_image",
+        value: {
+          src: "%URL%assets/1.png",
+          alt: "Call to action",
+        },
+      },
+    ],
+  },
+];
